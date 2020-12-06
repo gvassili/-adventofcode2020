1988ae37a571616deea12407fc03de9cef144483
Gwenael Vassilieff <gwenael@swapcard.com>
2020-12-06 17:38:41 +0100

    day05: switch to full input for part2 benchmark

--- calendar/day05/challenge_test.go
+++ calendar/day05/challenge_test.go
@@ -72,7 +72,7 @@ func TestChallenge_Part2(t *testing.T) {
 }
 
 func BenchmarkChallenge_Part2(b *testing.B) {
-	buf := bytes.NewReader([]byte(input))
+	buf := bytes.NewReader(fullInput)
 	var c Challenge
 	c.Prepare(buf)
 	for i := 0; i < b.N; i++ {
